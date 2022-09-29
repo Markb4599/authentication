9c53bac7aff9030cc86579a2c88ed650bfc8a2c8
Markb4599 <112779319+Markb4599@users.noreply.github.com>
2022-09-29 09:43:30 -0400

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+using WebApplication5_login.Models;
+
+namespace WebApplication5_login.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Secured()
+        {
+            return View();
+        }
+        [HttpGet("denied")]
+        public IActionResult Denied()
+        {
+            return View();
+        }
+
+
+        [Authorize(Roles ="Admin")]
+
+        public IActionResult Secured()
+        {
+            return View();
+        }
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost("login")]
+        public async Task<IUrlHelper> Validate(string username, string password, string returnUrl)
+        {
+            ViewData["ReturnUrl"]= returnUrl Url;
+            if (username == "coach" && password == "brown")
+            {
+                var claims = new List<Claim>();
+                claims.Add(new Claim("username", username));
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, username));
+                claims.Add(new Claim(ClaimTypes.Name, "Coach Brown"));
+                claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                await HttpContext.SingInAsync(claimsPrincipal);
+                return Redirect(returnUrl);
+            }
+            TempData["Error"] = "Error. Username/Password is invalid";
+            return View("login");
+        }
+
+            [Authorize]
+            public async Task<IActionResult> Logout()
+            {
+            await HttpContext.SignOutAsync();
+            return Redirect("/");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ startup.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Authn
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login";
+                    options.AccessDeniedPath = "/denied";
+                    options.Events = new CookieAuthenticationEvents()
+                    {
+                        OnSigningIn = async context =>
+                        {
+                            var principal = context.Principal;
+                            if (principal.HasClaim(c => c.Type == ClaimTypes.NameIdentifier))
+                            {
+                                if(principal.Claims.FirstOrDefault(c =>c.Type==ClaimTypes.NameIdentifier).Value == "bob")
+                                {
+                                    var.claimsIdentity = principal.Identity as ClaimsIdentity;
+                                    ClaimsIdentity.AddClaim(new Claim(ClaimTypes.Role, "Admin"));
+                                }
+                            }
+
+                            await Task.CompletedTask;
+                        },
+                        OnSignedIn = async context =>
+                        {
+                            await Task.CompletedTask;
+                        }
+                    };
+                    ///OnValidationPrincipal = async context =>
+                    //{
+                    //    await Task.CompletedTask;
+                    //}
+                });
+        }
+    }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            App.UseExceptionHandler("/Home/Error");
+            App.UseHsts();
+        }
+    }
+    
+        
+App.UseHttpRedirection();
+
+App.UseStaticFiles();
+
+App.UseRouting();
+App.UseAuthentication();
+App.UseAuthorization();
+
+App.UseEndpoints(Endpoints =>
+{
+    Endpoints.MapControllerRoute(
+        name: "default",
+        Pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+
+}
+}
+}
+
+
+
+namespace WebApplication5_login
+{
+    public class Startup
+    {
+    }
+}
+
